09e8aa3cd7ba669a3dd56680db90a5b1653a821f
mika <mika.mnurmasheva.mika@mail.ru>
2023-09-23 20:15:46 +0600

    Task 69

--- Task_69/Program.cs
+++ Task_69/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+int n = 10;
+int[] array = new int[n];
+bool Check(int[]array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        if(array[i]> array[i + 1]) return false;
+    }
+    return true;
+}
+
+for (int i = 0; i < n; i++) array[i]=Random.Shared.Next(100);
+Console.WriteLine($"[{String.Join(',' ,array)}]");
+Console.WriteLine(Check(array));
+for (int k=0; k<n-1; k++)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+    if (array[i] > array[i + 1])
+        {
+        int temp = array[i];
+        array[i] = array[i+1];
+        array[i + 1] = temp;
+        }
+    }
+}
+
+Console.WriteLine($"[{String.Join(',' ,array)}]");
+Console.WriteLine(Check(array));
